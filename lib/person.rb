a02b48967c2c2dbcbc729d06243e6f32a1e3f5ef
Brad Hemmerick <brad.hemmerick13@gmail.com>
2019-10-31 16:59:12 -0500

    some tests done

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,32 @@
-# your code goes here
+class Person
+    attr_accessor :bank_account
+    attr_reader :name, :happiness, :hygiene
+    
+    def hygiene=(hygiene)
+        @hygiene = hygiene
+        if hygiene > 10
+            @hygiene = 10
+        elsif hygiene < 0
+            @hygiene = 0
+        end
+    
+    end
+    
+    def happiness=(happiness)
+        @happiness = happiness
+        if happiness > 10
+            @happiness = 10
+        elsif happiness < 0
+            @happiness = 0
+        end
+    
+    end
+    
+    def initialize(name,happiness=8)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+end
+
